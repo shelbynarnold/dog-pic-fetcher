20d08e9c002ceec6afcb7dbab6dd757afbbbb2de
shelby <shelbynarnold@yahoo.com>
2023-03-09 23:09:20 -0700

    completed project

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
             .then((response) => response.json())
             .then((data) => {
                 let dogPics = document.createElement("img")
-                dogPics.setAttribute("src", data.message)  
+                dogPics.setAttribute("src", data.message)  //info abt breeds and random pic
                 dogPics.setAttribute("id" , "dogs")   
-                document.getElementById("body").append(dogPics)
+                document.getElementById("pics").append(dogPics)
             })
         
         let breedList = document.createElement('ul')
-        document.getElementById("body").append(breedList)  
+        document.getElementById("pics").append(breedList)  
             
         fetch("https://dog.ceo/api/breeds/list/all")
             .then((response) => response.json())
             .then((data) => {
-                    console.log(data)
                 for (const breed in data.message) {
-                    let x = document.createElement("button")
+                    let x = document.createElement("button") //breed button
                     x.setAttribute("id" , "bttn")
                     x.innerHTML = breed
                     let listItem = document.createElement('li')
@@ -30,18 +29,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
             fetch(`https://dog.ceo/api/breed/${dog}/images/random`)
             .then((response) => response.json())
             .then((data) => {
-                const breedPics = document.getElementById("dogs")
+                const breedPics = document.getElementById("dogs")  //replaces main pic
                 breedPics.setAttribute("src", data.message) 
             })
         }
+          let form = document.getElementById("form");
+          form.addEventListener("submit", function (e) {
+            e.preventDefault()
+            let commentBoxValue = document.getElementById("comment-box").value; //creates comment
+            form.reset()
+          
+            let li = document.createElement("li");
+            let text = document.createTextNode(commentBoxValue); //appends comment
+            li.appendChild(text);
+            document.getElementById("unordered-list").appendChild(li);
+          });
 
-    let post = document.getElementById("post");
-    post.addEventListener("click", function () {
-      let commentBoxValue = document.getElementById("comment-box").value;
-    
-      let li = document.createElement("li");
-      let text = document.createTextNode(commentBoxValue);
-      li.appendChild(text);
-      document.getElementById("unordered").appendChild(li);
-    });
-    })
+        let value = 0
+        const test = document.getElementById('like-button')
+        test.addEventListener('mouseover', () => {          //like button
+         value ++;
+         test.innerHTML = `${value} likes`
+        })
+
+        })
